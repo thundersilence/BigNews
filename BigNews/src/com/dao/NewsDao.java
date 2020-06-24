2be199469387c54b6b24bea221dd56a5f50bf159
wkh000 <1264071294@qq.com>
2020-06-24 15:20:47 +0800

    wkh
    修改update函数使更改不改变数据的时间，新增checkid函数和classified函数

--- BigNews/src/com/dao/NewsDao.java
+++ BigNews/src/com/dao/NewsDao.java
@@ -2,7 +2,9 @@ package com.dao;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import com.entity.News;
@@ -11,7 +13,6 @@ import com.utils.NewsDBUtils;
 
 public class NewsDao extends NewsDBUtils{
 	public int insert(News news) {//插入新的news到数据库，News共11个属性，其中10个需要赋值，具体要求在News中
-		
 		//给占位符赋予的值
 		Object params [] 
 				= {news.getId(),news.getName(),news.getTime(),news.getSource(),news.getContent(),news.getCommentNum(),news.getStars(),news.getPictureURL(),news.getSimple(),news.getAuthor(),news.getType()};
@@ -42,10 +43,11 @@ public class NewsDao extends NewsDBUtils{
 	public int update(News news) {//输入新的news，将news_id与news对象的id相同的数据库中的纪录的其他属性改为news对象中的属性
 		// 给占位符赋予的值
 		Object params [] 
-				= {news.getName(),news.getTime(),news.getSource(),news.getContent(),news.getCommentNum(),news.getStars(),news.getPictureURL(),news.getSimple(),news.getAuthor(),news.getType(),news.getId()};
+				= {news.getName(),news.getSource(),news.getContent(),news.getCommentNum(),news.getStars(),news.getPictureURL(),news.getSimple(),news.getAuthor(),news.getType(),news.getId()};
 		// 要执行的sql语句
-		String sql = "update news set news_name=?,news_time=?,news_source=?,news_content=?,news_comment_num=?,news_stars=?,news_pictureURL=?,news_simple=?,news_author=?,news_type=? where news_id=?";
+		String sql = "update news set news_name=?,news_source=?,news_content=?,news_comment_num=?,news_stars=?,news_pictureURL=?,news_simple=?,news_author=?,news_type=? where news_id=?";
 		// 执行sql语句
+		
 		int i = doUpdate(sql, params);
 
 		// 释放资源
@@ -55,7 +57,7 @@ public class NewsDao extends NewsDBUtils{
 	
 	public ArrayList<News> selectAll() {//返回数据库中所有的纪录，以arraylist<News>的形式返回
 		// 给占位符赋予的值
-		//Object params[] = null;
+		Object params[] = null;
 		// 要执行的sql语句
 		String sql = "select * from news";
 		// 执行sql语句
@@ -96,5 +98,60 @@ public class NewsDao extends NewsDBUtils{
 		getClose();
 		return list;
 	}
+	
+	public ArrayList<News> classified(String type) {  //返回分类新闻对象，输入为String,返回类型为该字符串的新闻对象
+		Object params[] = {type};
+		String sql = "select * from news where news_type=?";
+		ResultSet rs = doQuery(sql, params);
 
+		ArrayList<News> list = null;
+		
+		try {
+			if (rs.next()) {//判断是否至少存在一条数据记录
+				rs.beforeFirst();//将光标移动到第一行数据之前
+				list = new ArrayList<News>();
+				
+				//将学生存放在list集合中
+				while (rs.next()) {
+				
+					News news = new News();
+							news.setId(rs.getInt(1));
+							news.setName(rs.getString(2));
+							news.setTime(rs.getString(3));
+							news.setSource(rs.getString(4));
+							news.setContent(rs.getString(5));
+							news.setCommentNum(rs.getInt(6));
+							news.setStars(rs.getInt(7));
+							news.setPictureURL(rs.getString(8));
+							news.setSimple(rs.getString(9));
+							news.setAuthor(rs.getString(10));
+							news.setType(rs.getString(11));
+					
+					list.add(news);
+				}
+			}
+			 
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// 释放资源
+		getClose();
+		return list;
+	}
+
+	public boolean checkID(int id){ //验证id是否存在，存在返回false，不存在返回true
+		Object params[] = {id};
+		String sql = "select * from news where news_id=?";
+		ResultSet rs = doQuery(sql, params);
+		try {
+			if (rs.next())
+			return false;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return true;
+		
+	}
 }
--- BigNews/src/com/test/newsDaoTest.java
+++ BigNews/src/com/test/newsDaoTest.java
@@ -23,13 +23,13 @@ public void InsertTest() {
 	n.setAuthor("author");
 	n.setCommentNum(0);
 	n.setContent("content");
-	n.setId(5);
+	n.setId(6);
 	n.setName("name");
 	n.setPictureURL("pictureURL");
 	n.setSimple("simple");
 	n.setSource("source");
 	n.setStars(0);
-	n.setType("type");
+	n.setType("science");
 	int i=a.insert(n);
 	assertEquals(1, i);
 }
@@ -54,18 +54,48 @@ public void updateTest() {
 	n.setSimple("a");
 	n.setSource("a");
 	n.setStars(0);
-	n.setType("a");
+	n.setType("science");
 	int i=a.update(n);
 	assertEquals(1,i);
 }
 
 @Test 
-public void selectAll() {
+public void TestselectAll() {
 	ArrayList<News> n=a.selectAll();
 	News n1=new News();
     n1=n.get(1);
     int i=n1.getId();
     System.out.println(n1.getId());
     assertEquals(3,i);
+}
+
+@Test
+public void Testclassified() {
+	ArrayList<News> n=a.classified("science");
+	show(n);
+}
+
+public void show(ArrayList<News> n) {
+	News nn=new News();
+	for(int i=0;i<n.size();i++) {
+		nn=n.get(i);
+		System.out.println(nn.getAuthor());
+		System.out.println(nn.getCommentNum());
+		System.out.println(nn.getContent());
+		System.out.println(nn.getId());
+		System.out.println(nn.getName());
+		System.out.println(nn.getPictureURL());
+		System.out.println(nn.getSimple());
+		System.out.println(nn.getSource());
+		System.out.println(nn.getStars());
+		System.out.println(nn.getTime());
+		System.out.println(nn.getType());
+		}
+}
+
+@Test
+public void TestcheckId() {
+	boolean flag=a.checkID(1);
+	System.out.println(flag);
 }
 }
